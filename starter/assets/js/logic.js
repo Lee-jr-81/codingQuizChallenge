7a3ad6a005f16a09f880643fbec99fad5d55e029
Lee-jr-81 <129674277+Lee-jr-81@users.noreply.github.com>
2023-09-03 16:32:56 +0100

    working on the logic

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,21 +1,110 @@
-// PSEUDO COMMENTS FOR EACH STAGE
-// Variable setups
+//
+// QUESTION SETUP
+let questionBank = [
+  {
+    // Setup objects for each of my questions. Containing the properties i require.
+    question: "Question 1: Javascript is an ____ language?",
+    // Setup a nested array inside the parent array containing the four options.
+    options: [
+      "A. Object-Orientated",
+      "B. Object-Based",
+      "C. Procedural",
+      "D. None of the above",
+    ],
+    answer: "A",
+  },
+  {
+    question:
+      "Which of the following keywords is used to define a variable in javascript?",
+    options: ["A. var", "B. let", "C. Both A and B", "D. None of the above"],
+    answer: "C",
+  },
+  {
+    question:
+      "Which of the following methods is used to access HTML elements using Javascript?",
+    options: [
+      "A. getElementById()",
+      "B. getElementsByClassName()",
+      "C. Both A and B",
+      "D. None of the above",
+    ],
+    answer: "C",
+  },
+  {
+    question: "How can a data type be declared to be a constant type?",
+    options: ["A. const", "B. var", "C. let", "D. constant"],
+    answer: "A",
+  },
+  {
+    question:
+      "What keyword is used to check whether a given property is valid or not?",
+    options: ["A. in", "B. is in", "C. exists", "D. lies"],
+    answer: "A",
+  },
+  {
+    question:
+      "When an oprators value is NULL, the typeOf returned by the unary operator is:",
+    options: ["A. Boolean", "B. Undefined", "C. Object", "D. Integer"],
+    answer: "C",
+  },
+  {
+    question: "What does the 'toLocateString()' method do in Javascript?",
+    options: [
+      "A. returns a localised object representation",
+      "B. Returns a parsed string",
+      "C. Returns a localised string representation of an object",
+      "D. None of the above",
+    ],
+    answer: "C",
+  },
+];
+
+// INITIALISE VARIABLES
+//
+// GENERAL VARIABLE SETUPS
 let startButtonPress = document.querySelector("#start");
 let timerCountdown = document.querySelector("#time");
+let buttonChoice = document.getElementById("choice");
 //
+//
+// GAME VARIABLE SETUPS
+let secondsLeft = 60;
+let questionNumber = 0;
+let totalScore = 0;
+let questionCount = 1;
+//
+//
+// SECTION TOGGLE VARIABLES
+let toggleStartScreen = document.getElementById("start-screen");
+let toggleQuestions = document.getElementById("questions");
+let toggleEndScreen = document.getElementById("end-screen");
+let answerShow = document.getElementById("answer-title");
+//
+//
+// QUESTION BUTTON SELECTOR VARIABLES
+let quizQuestion = document.getElementById("question-title");
+let answerBtn1 = document.querySelector(".select-a");
+let answerBtn2 = document.querySelector(".select-b");
+let answerBtn3 = document.querySelector(".select-c");
+let answerBtn4 = document.querySelector(".select-d");
 //
 //
 //
 // A start button that when clicked starts a timer and shows the first question.
-startButtonPress.addEventListener("click", function (event) {
+startButtonPress.addEventListener("click", function () {
   quizTimer();
   questionToggle();
+  injectQuestions(questionNumber);
+  answerSelect();
 });
 
 // Need a function to toggle questions visible or not.
 function questionToggle() {
-  let toggle = document.getElementById("questions");
-  toggle.style.display = toggle.style.display == "block" ? "none" : "block";
+  // Used a ternary statement here to toggle between states.
+  toggleQuestions.style.display =
+    toggleQuestions.style.display == "block" ? "none" : "block";
+  toggleStartScreen.style.display =
+    toggleStartScreen.style.display == "none" ? "block" : "none";
 }
 
 // Create a function for the countdown clock.
@@ -36,7 +125,27 @@ function quizTimer() {
   }, 1000);
 }
 
+// Function to produce the questions and answers.
+function injectQuestions(n) {
+  quizQuestion.textContent = questionBank[n].question;
+  answerBtn1.textContent = questionBank[n].options[0];
+  answerBtn2.textContent = questionBank[n].options[1];
+  answerBtn3.textContent = questionBank[n].options[2];
+  answerBtn4.textContent = questionBank[n].options[3];
+  questionNumber = n;
+}
 //
+//
+// Question choice button press function containing conditional logic
+function answerSelect(event) {
+  if (questionBank[questionNumber].answer === event.target.value) {
+    answerShow.textContent = "Correct";
+    totalScore++;
+  } else {
+    answerShow.textContent = "Incorrect";
+    secondsLeft = secondsLeft - 10;
+  }
+}
 //
 //
 // Question contain buttons for each answer.
---
+++ starter/assets/js/questions.js
@@ -0,0 +1,59 @@
+// Initialise an array containing my questions
+let questionBank = [
+  {
+    // Setup objects for each of my questions. Containing the properties i require.
+    question: "Question 1: Javascript is an ____ language?",
+    // Setup a nested array inside the parent array containing the four options.
+    options: [
+      "A. Object-Orientated",
+      "B. Object-Based",
+      "C. Procedural",
+      "D. None of the above",
+    ],
+    answer: "A",
+  },
+  {
+    question:
+      "Which of the following keywords is used to define a variable in javascript?",
+    options: ["A. var", "B. let", "C. Both A and B", "D. None of the above"],
+    answer: "C",
+  },
+  {
+    question:
+      "Which of the following methods is used to access HTML elements using Javascript?",
+    options: [
+      "A. getElementById()",
+      "B. getElementsByClassName()",
+      "C. Both A and B",
+      "D. None of the above",
+    ],
+    answer: "C",
+  },
+  {
+    question: "How can a data type be declared to be a constant type?",
+    options: ["A. const", "B. var", "C. let", "D. constant"],
+    answer: "A",
+  },
+  {
+    question:
+      "What keyword is used to check whether a given property is valid or not?",
+    options: ["A. in", "B. is in", "C. exists", "D. lies"],
+    answer: "A",
+  },
+  {
+    question:
+      "When an oprators value is NULL, the typeOf returned by the unary operator is:",
+    options: ["A. Boolean", "B. Undefined", "C. Object", "D. Integer"],
+    answer: "C",
+  },
+  {
+    question: "What does the 'toLocateString()' method do in Javascript?",
+    options: [
+      "A. returns a localised object representation",
+      "B. Returns a parsed string",
+      "C. Returns a localised string representation of an object",
+      "D. None of the above",
+    ],
+    answer: "C",
+  },
+];
